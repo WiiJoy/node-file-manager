044ea4f2f8871a6f375c60b0b6de1bbb5d2d6016
Vitalii Rogozin <wiification@gmail.com>
2025-05-02 14:14:11 +0500

    feat: handle exit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import { argv } from 'node:process'
+import { argv, stdout, stdin } from 'node:process'
+import { EOL } from 'node:os'
 
 const filesManager = () => {
     const userString = argv.filter(arg => arg.startsWith('--username'))
     const username = userString[0].split('=')[1]
-    console.log('Welcome to the File Manager, ', username)
+
+    const readStream = stdin
+
+    readStream.on('data', (chunk) => {
+        const string = chunk.toString().trim()
+        console.log('string', string)
+        const command = string.split(' ')[0]
+
+        switch (command) {
+            case '.exit':
+                process.exit()
+            default:
+                stdout.write(`Invalid input${EOL}`)
+                break
+        }
+    })
+
+    stdout.write(`Welcome to the File Manager, ${username}${EOL}`)
+
+    process.on('SIGINT', () => process.exit())
+    process.on('exit', () => {
+        stdout.write(`${EOL}Thank you for using File Manager, ${username}, goodbye!${EOL}`)
+    })
     
 }
 
