58c3c0ae18f4d7ce1fd99acd00c3b77bacb63c00
Vitalii Rogozin <wiification@gmail.com>
2025-05-02 13:48:42 +0500

    feat: output welcome message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-// here will be the solution
+import { argv } from 'node:process'
+
+const filesManager = () => {
+    const userString = argv.filter(arg => arg.startsWith('--username'))
+    const username = userString[0].split('=')[1]
+    console.log('Welcome to the File Manager, ', username)
+    
+}
+
+filesManager()
